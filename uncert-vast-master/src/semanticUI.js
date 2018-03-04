7bc6ba9dc09ad421d9eaf475a127237752535934
Jing <jinli@ucdavis.edu>
2018-03-04 15:18:04 -0800

    add semanticUI

---
+++ uncert-vast-master/src/semanticUI.js
@@ -0,0 +1,27 @@
+$('.ui.accordion')
+  .accordion();
+
+$('.ui.dropdown')
+  .dropdown();
+
+// $('#classNum')
+//   .dropdown({
+//     values: [{
+//         name: '2',
+//       },
+//       {
+//         name: '3'
+//       },
+//       {
+//         name: '4',
+//       }
+//     ]
+//   });
+
+$('.activating.element')
+  .popup();
+
+$('#className')
+  .popup({
+    on: 'focus'
+  });
